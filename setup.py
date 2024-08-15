0078f2406f653bc514d6fbdcf37aef6ba90dda1e
James Cherti <60946298+jamescherti@users.noreply.github.com>
2024-08-14 22:57:10 -0400

    Enhance setup.py

--- setup.py
+++ setup.py
@@ -17,12 +17,20 @@
 # this program. If not, see <https://www.gnu.org/licenses/>.
 #
 
+from pathlib import Path
+
 from setuptools import find_packages, setup
 
 setup(
     name="pathaction",
     version="0.9.5",
     packages=find_packages(),
+    description="Rules to execute commands on any file",
+    long_description=((Path(__file__).parent.resolve().joinpath("README.md"))
+                      .read_text(encoding="utf-8")),
+    long_description_content_type="text/markdown",
+    url="https://github.com/jamescherti/pathaction",
+    author="James Cherti",
     setup_requires=[
         "pytest",
         "pytest-cov",
@@ -30,10 +38,31 @@ setup(
     install_requires=[
         "colorama",
         "jinja2",
-        # "schema==v0.7.5",
         "schema",
         "PyYAML",
         "setproctitle",
+    ],
+    python_requires=">=3.6, <4",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "Topic :: Utilities",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: System :: Systems Administration",
+        "Topic :: System :: Filesystems",
+        "Topic :: System :: Software Distribution",
+        "Environment :: Console",
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: POSIX :: Other",
     ],
     entry_points={
         "console_scripts": [
