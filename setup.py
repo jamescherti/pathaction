9f5bff3934a87fbe3bbe046536cd23de7bfc00ca
James Cherti <60946298+jamescherti@users.noreply.github.com>
2025-06-26 10:50:51 -0400

    Release: 0.9.6

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="pathaction",
-    version="0.9.5",
+    version="0.9.6",
     packages=find_packages(),
     description="Rules to execute commands on any file",
     long_description=((Path(__file__).parent.resolve().joinpath("README.md"))
