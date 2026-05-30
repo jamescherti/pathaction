991f9c93658bbc70f5525cd7647d7e26396aaa71
James Cherti <60946298+jamescherti@users.noreply.github.com>
2026-05-30 14:30:13 -0400

    Release: 1.0.1

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="pathaction",
-    version="1.0.0",
+    version="1.0.1",
     packages=find_packages(),
     description=("A universal Makefile for any file in the filesystem: "
                  "Rule-driven commands for any file or directory"),
